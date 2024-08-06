3e0ad68624bb03b0f6325646dd2dee4165785ee5
Rubayat Areen <rubayatareen23@gmail.com>
2024-08-06 18:33:45 -0500

    Add files via upload

---
+++ parser.h
@@ -0,0 +1,110 @@
+#pragma once
+
+#include <variant>
+
+#include "tokenization.h"
+
+struct NodeExprIntLit {
+    Token int_lit;
+};
+
+struct NodeExprIdent {
+    Token ident;
+};
+
+struct NodeExpr {
+    std::variant<NodeExprIntLit, NodeExprIdent> var;
+};
+
+struct NodeStmtExit {
+    NodeExpr expr;
+};
+
+struct NodeStmtLet {
+    Token ident;
+    NodeExpr expr;
+};
+
+struct NodeStmt {
+    std::variant<NodeStmtExit, NodeStmtLet> var;
+};
+
+struct NodeProg {
+    std::vector<NodeStmt> stmts;
+};
+
+class Parser {
+public:
+    inline explicit Parser(std::vector<Token> tokens)
+        : m_tokens(std::move(tokens)) {}
+
+    std::optional<NodeExpr> parse_expr() {
+        if (peek().has_value() && peek().value().type == TokenType::int_lit) {
+            return NodeExpr{.var = NodeExprIntLit {.int_lit =  consume () }};
+        }
+        else if (peek().has_value() && peek().value().type == TokenType::ident) {
+            return NodeExpr {.var = NodeExprIdent {.ident = consume() }};
+        }
+        else {
+            return {};
+        }
+    }
+
+    std::optional<NodeStmt> parse_stmt() {
+        if (peek().has_value() && peek().value().type == TokenType::exit) {
+            
+        }
+    }
+
+
+    std::optional<NodeExit> parse() {
+        std::optional<NodeExit> exit_node;
+        while(peek().has_value()) {
+            if (peek().value().type == TokenType::exit && peek(1).has_value() && peek(1).value().type == TokenType::open_paren) {
+                consume();
+                consume();
+                if (auto node_expr = parse_expr()) {
+                    exit_node = NodeExit{.expr = node_expr.value()};
+                }
+                else {
+                    std::cerr << "Invalid expression" << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+                if (peek().has_value() && peek().value().type == TokenType::close_paren) {
+                    consume();
+                } else {
+                    std::cerr << "Expected ')'" << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+                if (peek().has_value() && peek().value().type == TokenType::semi) {
+                    consume();
+                } else {
+                    std::cerr << "Expected ';'" << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+            }
+        }
+        m_index = 0;
+        return exit_node;
+    }
+
+
+private:
+
+    [[nodiscard]] inline std::optional<Token> peek(int offset = 0) const
+    {
+        if (m_index + offset >= m_tokens.size()) {
+            return {};
+        }
+        else {
+            return m_tokens.at(m_index + offset);
+        }
+    }
+
+    inline Token consume() {
+        return m_tokens.at(m_index++);
+    }
+
+    const std::vector<Token> m_tokens;
+    size_t m_index = 0;
+};
